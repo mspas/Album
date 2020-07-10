98a823ec0be61956ff4b77b1174e06ec40816e5a
Marcin Spasinski <marcin7789@gmail.com>
2020-07-10 19:47:54 +0200

    login view

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -3,7 +3,8 @@ import { Form, Button, InputGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import AuthService from "../services/auth.service";
-import "../styles/SignIn.module.sass";
+import styles from "../styles/SignIn.module.sass";
+import logo from "../assets/logo-text.png";
 
 function SignIn(props) {
   const [password, setPassword] = useState("");
@@ -26,32 +27,42 @@ function SignIn(props) {
   };
 
   return (
-    <Form className="form-signin" onSubmit={onSubmit}>
-      <Form.Group controlId="formPassword">
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroupPrepend">
-              <FontAwesomeIcon className="panel-icon" icon={faLock} />
-            </InputGroup.Text>
-          </InputGroup.Prepend>
-          <Form.Control
-            type="password"
-            name="Password"
-            placeholder="Password"
-            aria-describedby="inputGroupPrepend"
-            onChange={inputChangeHandler}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please type your password.
-          </Form.Control.Feedback>
-        </InputGroup>
-      </Form.Group>
+    <div className={styles.signIn + " center"}>
+      <div className={styles.panelSignIn}>
+        <div className={styles.header}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <Form className={styles.formSignIn} onSubmit={onSubmit}>
+          <Form.Group controlId="formPassword">
+            <span className={styles.info}>
+              Album Paradyż - panel administratora
+            </span>
+            <InputGroup className={styles.input}>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroupPrepend">
+                  <FontAwesomeIcon className="panel-icon" icon={faLock} />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                type="password"
+                name="Password"
+                placeholder="Hasło"
+                aria-describedby="inputGroupPrepend"
+                onChange={inputChangeHandler}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please type your password.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
+          <Button variant="primary" type="submit">
+            Zaloguj
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
 
